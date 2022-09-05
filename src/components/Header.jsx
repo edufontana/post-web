bba9e383fff9473af2a488faa6c77af2fd7177ee
edu <edufontana7@gmail.com>
2022-09-05 13:22:31 -0300

    style: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
 
+import IgniteLogo from '../assets/ignite-logo.svg';
+
 export function Header(){
   return(
     <header className={styles.header}>
-      <strong>Feed</strong>
+      <img src={IgniteLogo} alt="Logotipo" />
     </header>
   )
 }
