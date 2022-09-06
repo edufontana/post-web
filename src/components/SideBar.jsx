38b515b36a1a33947b679d2c4952040585e104c1
edu <edufontana7@gmail.com>
2022-09-06 10:24:12 -0300

    style: sidebar-avatar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import styles from './SideBar.module.css'
+import { PencilLine } from 'phosphor-react';
 
 export function SideBar(){
 
@@ -10,6 +11,8 @@ export function SideBar(){
       src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"  />
 
       <div className={styles.profile}>
+      <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/46920937?v=4"/>
+
         <strong>Eduardo</strong>
         <span>Web Developer</span>
       </div>
@@ -17,7 +20,8 @@ export function SideBar(){
 
       <footer>
         <a href="#">
-          Editar seu Perfil
+          <PencilLine size={20}/>
+          Editar seu perfil
         </a>
       </footer>
     </aside>
